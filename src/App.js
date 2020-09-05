280a49b04b9f80318fe3bf215e2f249a7f508787
Sohaib Mubashir <sohaibmh@yahoo.com>
2020-09-05 23:00:45 +0100

    second commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="mainContainer">
+      <div className="boardsContainer">
+        <div className="board-1"></div>
+        <div className="board-2"></div>
+        <div className="board-3"></div>
+        <div className="board-4"></div>
+        <div className="board-5"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
