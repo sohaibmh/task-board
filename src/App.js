f9e7fd5a4bb183b6adb0a5a6ea02fb764efb17ff
Sohaib Mubashir <sohaibmh@yahoo.com>
2020-09-07 07:03:02 +0100

    added basic structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ function App() {
   return (
     <div className="mainContainer">
       <div className="boardsContainer">
-        <div className="board-1"></div>
+        <div className="board-1">
+          <div className="card-1" draggable>
+            Name: Sohaib
+            <br />
+            Address: 250 Church Hill Road
+          </div>
+        </div>
         <div className="board-2"></div>
         <div className="board-3"></div>
         <div className="board-4"></div>
